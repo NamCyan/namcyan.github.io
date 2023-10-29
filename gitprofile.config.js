badd712d8761585245830e11f5c4242d1eb46412
NamCyan <nkimxte@gmail.com>
2023-10-29 12:51:36 +0700

    update

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -126,7 +126,7 @@ const config = {
       conference: "[JCSCE] VNU Journal of Science: Computer Science and Communication Engineering",
       description:'vlsp-2021 | machine-reading-comprehension | vietnamese | retrospective-reader',
       keywords: ['vlsp-2021', 'machine-reading-comprehension', 'vietnamese', 'retrospective-reader'],
-      imageUrl: './public/thevault.jpg',
+      imageUrl: 'public/thevault.png',
       link: 'https://jcsce.vnu.edu.vn/index.php/jcsce/article/view/346',
     },
   ],
